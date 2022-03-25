a5fd9712f7d26e1b9f88c5d0daa791450abaafa4
KaixiangYang6 <kaixiang.yang.work@gmail.com>
2022-03-25 16:29:04 +0000

    update

--- Python Instructions.md
+++ Python Instructions.md
@@ -17,14 +17,18 @@
 先在左侧extensions里搜索python，安装第一个扩展。
 找个地儿新建一个文件夹，在VS里打开这个文件夹，并在VS里新建一个空文件保存为.py格式。
 
+## Python 名词概念
+text data in python is called a string 字符串
+  
 ## Python 语法
-注释comments
+**注释comments**
 #………	#The comments start with a hash character/the pound sign
-变量习惯于用小写字母lowercase组成，如有多个单词，用下划线underscore隔开。名称尽可能具有描述性，说明性
+**变量习惯于用小写字母lowercase组成，如有多个单词，用下划线underscore隔开。**名称尽可能具有描述性，说明性
 
 每行不需要分号semicolon
 
 string字符串，用单引号single quote，如果文本包含单引号，则在单引号前添加反斜线\ backslash。 例如：’Bobby\’s World’，或者”Bobby’s World”达到同样效果。”””Bobby’s World”””将强制字符串，包括范围内的换行。
 
-## Python 名词概念
-text data in python is called a string 字符串
+Methods and functions are basically the same thing. Method is just a function that belong to an object
+## Function
+basic function.py
---
+++ basic function.py
@@ -0,0 +1,20 @@
+message = 'Hello World'
+print(len(message)) # 打印变量message的长度,return 11
+print(message[0]) 
+print(message[0:5]) #打印第一至第六个范围内的字母
+print(message[6:])  #打印第七至最后一个字母
+print(message.lower())  #变成小写字母
+print(message.upper())  #变成大写字母
+print(message.count('l'))   #计数l再变量message中出现的次数
+print(message.find('World'))    #查找World的位置，return 6，因为World从第六个开始。如果找不到，则return -1
+
+new_message = message.replace('World', 'Universe')  #需要新变量名继承变化，原变量无法更新。
+message = message.replace('World', 'Universe')  #需要重新设置变量，原变量无法自动更新改变。
+print(message)
+
+greeting = 'Hello'
+name = 'Michael'
+message = greeting + ', ' + name + '. Welcome '    #使用‘+’将文本连接，但不适合处理复杂文本
+message = '{}, {}. Welcome!'.format(greeting, name) #使用placeholder大括号占据空位，用format函数填充空位
+message = f'{greeting}, {name.upper()}. Welcome!'   #新的长字符串表达方式，用f，并将内容填写在大括号内，不再用format函数。并且可在大括号内继续使用其他函数
+print(message)
